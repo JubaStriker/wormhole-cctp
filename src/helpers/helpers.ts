3c4b7a25d3e88619561a836e65cc661dfec08fe0
JubaStriker <jubastriker@gmail.com>
2025-04-23 14:20:39 +0600

    Helper function updated for token transfer

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -5,9 +5,13 @@ import {
     Signer,
     Wormhole,
     Chain,
+    TokenId,
+    isTokenId,
 } from '@wormhole-foundation/sdk';
 import evm from '@wormhole-foundation/sdk/evm';
 import solana from '@wormhole-foundation/sdk/solana';
+import sui from '@wormhole-foundation/sdk/sui';
+import aptos from '@wormhole-foundation/sdk/aptos';
 import { config } from 'dotenv';
 config();
 
@@ -26,7 +30,8 @@ function getEnv(key: string): string {
 
 // Signer setup function for different blockchain platforms
 export async function getSigner<N extends Network, C extends Chain>(
-    chain: ChainContext<N, C>
+    chain: ChainContext<N, C>,
+    gasLimit?: bigint
 ): Promise<{
     chain: ChainContext<N, C>;
     signer: Signer<N, C>;
@@ -42,9 +47,24 @@ export async function getSigner<N extends Network, C extends Chain>(
             ).getSigner(await chain.getRpc(), getEnv('SOL_PRIVATE_KEY'));
             break;
         case 'Evm':
+            const evmSignerOptions = gasLimit ? { gasLimit } : {};
             signer = await (
                 await evm()
-            ).getSigner(await chain.getRpc(), getEnv('ETH_PRIVATE_KEY'));
+            ).getSigner(
+                await chain.getRpc(),
+                getEnv('ETH_PRIVATE_KEY'),
+                evmSignerOptions
+            );
+            break;
+        case 'Sui':
+            signer = await (
+                await sui()
+            ).getSigner(await chain.getRpc(), getEnv('SUI_MNEMONIC'));
+            break;
+        case 'Aptos':
+            signer = await (
+                await aptos()
+            ).getSigner(await chain.getRpc(), getEnv('APTOS_PRIVATE_KEY'));
             break;
         default:
             throw new Error('Unsupported platform: ' + platform);
@@ -55,4 +75,16 @@ export async function getSigner<N extends Network, C extends Chain>(
         signer: signer as Signer<N, C>,
         address: Wormhole.chainAddress(chain.chain, signer.address()),
     };
+}
+
+export async function getTokenDecimals<
+    N extends 'Mainnet' | 'Testnet' | 'Devnet'
+>(
+    wh: Wormhole<N>,
+    token: TokenId,
+    sendChain: ChainContext<N, any>
+): Promise<number> {
+    return isTokenId(token)
+        ? Number(await wh.getDecimals(token.chain, token.address))
+        : sendChain.config.nativeTokenDecimals;
 }
